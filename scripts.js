9172d5e1507461b7996c2a1a813042777e75706c
melodiefan123 <melodie.fan@gmail.com>
2025-01-27 19:57:28 -0800

    pathway fix

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@
 function redirect(item) {   
     console.log(item.value)
     console.log(item + ".html")
-    window.location.href= item.value + ".html"
+    window.location.href= "html/" + item.value + ".html"
 }
